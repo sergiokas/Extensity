c236bc62dca3fa99dada019933444b1d350312ef
sergiokas <dev@sergiokas.com>
2015-08-18 17:32:16 -0300

    additional profiles functionality

--- js/profiles.js
+++ js/profiles.js
@@ -3,9 +3,9 @@ jQuery(document).ready( function($) {
   _.mixin(_.string.exports());
 
   var templates = {
-    items: '<label><input type="checkbox" value="%s" /> <img src="%s" width="16px" height="16px" /> %s</label><br />'
+    profile: '<li><a href="javascript:void(0);" class="profile" data-name="%s">%s</a></li>',
+    extension: '<label><input type="checkbox" class="extension" value="%s" /> <img src="%s" width="16px" height="16px" /> %s</label><br />'
   }
-
 
   // Get the smallest icon URL available for a given extension.
   var smallestIconUrl = function(icons) {
@@ -14,15 +14,73 @@ jQuery(document).ready( function($) {
     return (icon && icon.url) || '';
   };
 
+  var profiles = JSON.parse(localStorage["profiles"] || "{}");
+
+  var refresh = function() {
+    $("#profiles #list #items").empty();
+    _(profiles).each(function(item, idx) {
+      $("#profiles #list #items").append(
+        _(templates.profile).sprintf(idx, idx)
+      )
+    });
+
+    if(!_(profiles).keys().length) {
+      // TODO: default new profile
+    }
+  };
+
+  var save = function() {
+    var selected = _($('#profiles input.extension:checked')).pluck('value');
+    var n = $("#profiles input#name").val();
+    profiles[n] = selected;
+    localStorage['profiles'] = JSON.stringify(profiles);
+    refresh();
+  }
+
+  var edit = function(name) {
+    $("#profiles #edit").hide();
+    $("#profiles #list #items li").removeClass("active");
+    $("#profiles input#name").val(name);
+    $("#profiles input.extension").prop("checked", false);
+    _(profiles[name]).each(function(item) {
+      $("#profiles input.extension[value='" + item +"'").prop("checked", true);
+    });
+    $("#profiles #edit").slideDown('fast');
+  }
+
+  refresh();
 
   // Init main extensions list
   chrome.management.getAll(function(results) {
+    results = _(results).chain().filter(function(i) { return i.type=='extension' && i.name !== 'Extensity'}).sortBy('name').value();
     _(results).each(function(item) {
-      console.log(item);
-      $("#profiles #edit").append(
-        _(templates.items).sprintf(item.id, smallestIconUrl(item.icons), _(item.name).prune(35))
-      );
+      if(item.type == 'extension') {
+        $("#profiles #edit").append(
+          _(templates.extension).sprintf(item.id, smallestIconUrl(item.icons), _(item.name).prune(35))
+        );
+      }
     });
+  });
+
+  // Bindings
+  $('#profiles #check-all').on('change', function(ev) {
+    ev.preventDefault();
+    $("#profiles input.extension").prop("checked", this.checked);
+  });
+
+  $('#profiles a.profile').on('click', function(ev) {
+    ev.preventDefault();
+    edit($(this).data("name"));
+  })
+
+  $('#profiles #save').on('click', function(ev) {
+    ev.preventDefault();
+    save();
+  });
+
+  $('.fa-close').on('click', function(ev) {
+    ev.preventDefault();
+    $(this).parent('p').slideUp();
   });
 
 });
